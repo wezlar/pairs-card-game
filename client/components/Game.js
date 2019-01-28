73571c580fd55df8f0b1261e470b8ca452145bac
Dean Pugh <dean.pugh@15gifts.com>
2019-01-28 08:17:36 +0000

    Begin startNewGame function to select random Cards

--- client/components/Game.js
+++ client/components/Game.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 class Game extends Component {
+  startNewGame () {
+    const { game: { numberOfCards }, cards } = this.props;
+    const halfNumberOfCards = numberOfCards / 2;
+
+    for (let i = 0; i < halfNumberOfCards; i++) {
+      const index = Math.floor((Math.random() * cards.length) + 1);
+      console.log(index);
+    }
+
+  }
+  
   render () {
+    this.startNewGame();
     return (
       <div>
         <p>Game</p>
-        { this.props.cards.map((card, i) => <Card key={ i } card={ card } />) }
+        
       </div>
     );
   }
