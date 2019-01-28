0bcdfac5c3050090029b6200b41cf4192cb34187
Dean Pugh <dean.pugh@15gifts.com>
2019-01-28 08:16:22 +0000

    Add in basic styling ready for animation

--- client/components/Card.js
+++ client/components/Card.js
@@ -1,4 +1,39 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+  position: relative;
+  width: 150;
+  height: 217px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+`;
+
+const CardBody = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  box-shadow: 3px 3px 3px rgba(0,0,0,0.2);
+  transform-style: preserve-3d;
+  transition: transform .3s ease-in-out;
+`;
+
+const CardBack = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  backface-visibility: hidden;
+  background: #333;
+`;
+
+const CardFront = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transition-delay: .15s;
+  z-index: 1;
+`;
 
 class Card extends Component {
   render () {
@@ -7,14 +42,16 @@ class Card extends Component {
     const file = require(`../${image}`);
 
     return (
-      <div>
-        <div className="card__face">
-        <g dangerouslySetInnerHTML={ { __html: file } } />
-        </div>
-        <div className="card__back">
-          BACK { name }
-        </div>
-      </div>
+      <CardWrapper>
+        <CardBody>
+          <CardBack className="card__back">
+            BACK { name }
+          </CardBack>
+          <CardFront className="card__face">
+            <div dangerouslySetInnerHTML={ { __html: file } } />
+          </CardFront>
+        </CardBody>
+      </CardWrapper>
     );
   }
 };
