d0b72824dc002179143b49dd27470621606031bd
Dean Pugh <dean.pugh@15gifts.com>
2019-03-15 08:12:38 +0000

    Add missing files from previous commit

---
+++ client/utils/newGame/newGame.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import newGame from './newGame';
+import cards from '../../data/cards'
+
+describe(`newGame util`, () => {
+  test(`Test that shuffled deck returned`, () => {
+    let newDeck = newGame(cards, 5);
+    expect(newDeck.length).toBe(6);
+    
+    newDeck = newGame(cards, 10);
+    expect(newDeck.length).toBe(10);
+
+    newDeck = newGame(cards, 20);
+    expect(newDeck.length).toBe(20);
+  });
+
+  test(`Test card has extra fields set`, () => {
+    let newDeck = newGame(cards, 10);
+    expect(newDeck[0].isFlipped).toBe(false);
+    expect(newDeck[0].hasMatched).toBe(false);
+    expect(newDeck[0].cardIndex).to
+  })
+});
