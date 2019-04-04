27ce7c166a94429387a4f34895ca3efa1b2541d8
Dean Pugh <dean.pugh@15gifts.com>
2019-04-04 07:45:29 +0100

    Add in test for actionCreators

---
+++ client/actions/actionCreators.test.js
@@ -0,0 +1,46 @@
+import ACTION_TYPES from './constants';
+import {
+  cardFlip,
+  cardMatched,
+  addNewDeck,
+  updateLastCard,
+  updateScore,
+  updateNumberOfCards,
+  updateCardsMatched,
+} from './actionCreators';
+
+import mockDeck from '../../testHelpers/__mocks__/mockDeck';
+
+describe(`actionCreators.js`, () => {
+  describe(`ACTION_TYPES`, () => {
+    test(`it matches the snapshot`, () => {
+      expect(ACTION_TYPES).toMatchSnapshot();
+    });
+  });
+
+  describe(`actions`, () => {
+    test(`cardFlip()`, () => {
+      const payload = 1;
+      expect(cardFlip(payload)).toMatchSnapshot();
+    });
+    test(`cardMatched()`, () => {
+      const payload = 1;
+      expect(cardMatched(payload)).toMatchSnapshot();
+    });
+    test(`addNewDeck()`, () => {
+      expect(addNewDeck(mockDeck)).toMatchSnapshot();
+    });
+    test(`updateLastCard()`, () => {
+      expect(updateLastCard(1, '2 of hearts')).toMatchSnapshot();
+    });
+    test(`updateScore()`, () => {
+      expect(updateScore(10)).toMatchSnapshot();
+    });
+    test(`updateNumberOfCards()`, () => {
+      expect(updateNumberOfCards(20)).toMatchSnapshot();
+    });
+    test(`updateCardsMatched()`, () => {
+      expect(updateCardsMatched(4)).toMatchSnapshot();
+    });
+  });
+});
