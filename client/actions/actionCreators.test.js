4a1af4aac1f03eb66df7b2920bcaca455f4642cd
Dean Pugh <deanpugh@deanpugh.com>
2019-04-10 08:11:48 +0100

    Add spaces between tests, as per Dales comment

--- client/actions/actionCreators.test.js
+++ client/actions/actionCreators.test.js
@@ -16,22 +16,28 @@ describe(`actionCreators.js`, () => {
       const payload = 1;
       expect(cardFlip(payload)).toMatchSnapshot();
     });
+
     test(`cardMatched()`, () => {
       const payload = 1;
       expect(cardMatched(payload)).toMatchSnapshot();
     });
+
     test(`addNewDeck()`, () => {
       expect(addNewDeck(mockDeck)).toMatchSnapshot();
     });
+
     test(`updateLastCard()`, () => {
       expect(updateLastCard(1, '2 of hearts')).toMatchSnapshot();
     });
+
     test(`updateScore()`, () => {
       expect(updateScore(10)).toMatchSnapshot();
     });
+
     test(`updateNumberOfCards()`, () => {
       expect(updateNumberOfCards(20)).toMatchSnapshot();
     });
+    
     test(`updateCardsMatched()`, () => {
       expect(updateCardsMatched(4)).toMatchSnapshot();
     });
